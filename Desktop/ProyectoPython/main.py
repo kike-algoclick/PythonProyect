b37b1578e0b3869e32a1e809f0f7db692b787335
kike-algoclick <aldo.barriere2026@hilasal.superate.org.sv>
2025-02-18 11:54:37 -0600

    add creation pairs

---
+++ Desktop/ProyectoPython/main.py
@@ -0,0 +1,48 @@
+#Liberia "random"|
+import random
+
+#Variables
+numbers = []
+highestScore = []
+
+
+
+#Creación de los pares de numeros aleatorios
+def createPairs():
+    for i in range(1,20):
+        num = random.randint(1,200)
+        pair = num
+        numbers.append(num)
+        numbers.append(pair)
+    random.shuffle(numbers)
+    print(numbers)
+
+def checkNumbers(a,b):
+    for i in numbers:
+        if a == b:
+            print("+5 puntos")
+
+
+while(True):
+    print("Bienvenido a tu juego de memoria de números \n")
+    print("Elige una opción:\n")
+    print("Opción 1: Partida Nueva")
+    print("Opción 2: Ver Puntuación Máxima")
+    print("Opción 3: salir")
+    
+    answer = input("Elige tu opción: \n" )
+
+    if answer == "1":
+        print("La partida ha empezado \n")
+        createPairs(
+            checkNumbers()
+        )
+    elif answer == "2":
+        print("Tu puntaje máximo es de: ",highestScore)
+    elif answer == "3":
+        print("Gracias por haber jugado")
+        break
+
+
+
+
