a7f4c7f1831e57b264cc5c55e2bb292034c908b8
kike-algoclick <aldo.barriere2026@hilasal.superate.org.sv>
2025-02-19 21:44:48 -0600

    add checkNumbers function, and while playing menu incomplete

--- Desktop/ProyectoPython/main.py
+++ Desktop/ProyectoPython/main.py
@@ -1,26 +1,61 @@
-#Liberia "random"|
+#Liberia "random"
 import random
 
 #Variables
 numbers = []
 highestScore = []
+score = []
 
 
 
 #Creación de los pares de numeros aleatorios
 def createPairs():
-    for i in range(1,20):
-        num = random.randint(1,200)
+    for i in range(1,5):
+        num = random.randint(1,10)
         pair = num
         numbers.append(num)
         numbers.append(pair)
     random.shuffle(numbers)
     print(numbers)
+    print("Debes encontrar", len(numbers), "parejas")
+    
 
-def checkNumbers(a,b):
-    for i in numbers:
-        if a == b:
-            print("+5 puntos")
+
+
+#función para chequear los pares de números dados por el usuario
+def checkNumbers():
+
+    a = int(input("Elige un número \n"))
+    b = int(input("Elige otro número"))
+    num1 = numbers[a]
+    num2 = numbers[b]
+    print(num1,num2)
+
+
+#Comprobar si los indices dados contienen el mismo números
+    while len(numbers) > 0 :
+        if num1 == num2:
+            puntos = 10
+            print("has encontrado  una pareja")
+            score.append(puntos)
+            print("has ganado 4 puntos")
+            
+        #Se remueven las parejas encontradas
+            numbers.remove(numbers[num1])
+            numbers.remove(numbers[num2])
+        elif num1 != num2:
+            print("Los numeros no coinciden vuelve a intentar")
+        elif num1 == "salir":
+            print("terminaste la partida")
+            print("tu puntaje fue", score)
+            score.clear()
+            print(score)
+            break
+    
+            
+
+            
+
 
 
 while(True):
@@ -35,12 +70,14 @@ while(True):
     if answer == "1":
         print("La partida ha empezado \n")
         createPairs()
+        checkNumbers()
+
+
     elif answer == "2":
         print("Tu puntaje máximo es de: ",highestScore)
     elif answer == "3":
         print("Gracias por haber jugado")
         break
-
 
 
 
