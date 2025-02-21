04d9a9c7c36ef61a1f1287ba93d0880b58d6ecd5
kike-algoclick <aldo.barriere2026@hilasal.superate.org.sv>
2025-02-21 17:25:44 -0600

    Added hig score funtion and score by play

--- Desktop/ProyectoPython/main.py
+++ Desktop/ProyectoPython/main.py
@@ -1,65 +1,99 @@
 #Liberia "random"
 import random
+import math
 
 #Variables
-numbers = []
-highestScore = []
-score = []
+highestScore = 0
 
 
 
 #Creación de los pares de numeros aleatorios
 def createPairs():
-    for i in range(1,5):
-        num = random.randint(1,10)
-        pair = num
+    numbers = []
+    for _ in range(10):
+        num = random.randint(1,30)
         numbers.append(num)
-        numbers.append(pair)
+        numbers.append(num)
     random.shuffle(numbers)
-    print(numbers)
-    print("Debes encontrar", len(numbers), "parejas")
+    return numbers
     
-
-
 
 #función para chequear los pares de números dados por el usuario
 def checkNumbers():
-
-    a = int(input("Elige un número \n"))
-    b = int(input("Elige otro número"))
-    num1 = numbers[a]
-    num2 = numbers[b]
-    print(num1,num2)
-
-
+    global highestScore
+    numbers = createPairs()
+    score = 0 
 #Comprobar si los indices dados contienen el mismo números
     while len(numbers) > 0 :
-        if num1 == num2:
-            puntos = 10
-            print("has encontrado  una pareja")
-            score.append(puntos)
-            print("has ganado 4 puntos")
-            
-        #Se remueven las parejas encontradas
-            numbers.remove(numbers[num1])
-            numbers.remove(numbers[num2])
-        elif num1 != num2:
-            print("Los numeros no coinciden vuelve a intentar")
-        elif num1 == "salir":
-            print("terminaste la partida")
-            print("tu puntaje fue", score)
-            score.clear()
-            print(score)
-            break
+        
+        try:
+            a = int(input("Elige un número entre 0 y {}: \n \n".format(len(numbers))))
+            b = int(input("Elige otro número entre 0 y {}: \n \n".format(len(numbers))))
+            num1 = numbers[a]
+            num2 = numbers[b]
+
+            if a == b:
+                print("Debes elegir 2 posiciones diferentes")
+                continue
+            if a < 0 or b < 0 or a >= len(numbers) or b >= len(numbers):
+                print("Índices fuera de rango. Intenta de nuevo.")
+                continue
+
+            num1 = numbers[a]
+            num2 = numbers[b]
+
+            if num1 == num2:
+        
+                print("elegiste", num1)
+                print("elegiste",num2)
+                score += 10
+                print("has encontrado una pareja! \n")
+                print("puntaje" , score ,"puntos \n")
+                print("has ganado 10 puntos")
+
     
+    
+        #Se remueven las parejas encontradas
+                numbers[a] = None
+                numbers[b] = None
+        #se iteran los números y elimina los elementos ya encontrados
+                numbers = [n for n in numbers if n is not None]
+                print("debes encontrar", round(len(numbers)/2), "parejas más")
+                
+    
+            elif num1 != num2:
+                print(num1)
+                print(num2)
+                print("Los numeros no coinciden vuelve a intentar")
+               
             
-
             
+        except ValueError:
+                ("Debes escoger números")
+                
+        number1 = str(num1)
+        number2 = str(num2)
 
+        if number1 == "salir":
+            print("seguro que quieres salir? Escribelo otra vez para confimar")
+        if number2 == "salir":
+                print("La partida he terminado")
+                print("tu puntaje es:", score)
+                break
+        
+        
+    ##puntuación maxima
+    if score > highestScore:
+        highestScore = score
+        print("tu puntaje maximo es:")
+    
+      
+    
+        
 
 
 while(True):
-    print("Bienvenido a tu juego de memoria de números \n")
+    print("Bienvenido a tu Memory Numbers \n")
     print("Elige una opción:\n")
     print("Opción 1: Partida Nueva")
     print("Opción 2: Ver Puntuación Máxima")
@@ -68,8 +102,6 @@ while(True):
     answer = input("Elige tu opción: \n" )
 
     if answer == "1":
-        print("La partida ha empezado \n")
-        createPairs()
         checkNumbers()
 
 
