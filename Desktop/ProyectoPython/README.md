59526d30cad750261e54eefb62797a169ed8cbb4
kike-algoclick <aldo.barriere2026@hilasal.superate.org.sv>
2025-02-21 23:43:08 -0600

    Manual added

---
+++ Desktop/ProyectoPython/README.md
@@ -0,0 +1,13 @@
+#El siguiente programa es un juego de memoria de números 
+#Al iniciar el programa verás un menú que tendra 3 opciones
+-Partida nueva: este opción comenzara la partida donde deberas adivinar las parejas
+-Ver Puntuación máxima: esta opción te mostrara tu puntaje más alto obtenido 
+-Salir: si escoges este saldras del programa o juego
+
+
+#Al jugar:
+Cuando inicies una partida el programa creara un número de parejas aleatorio y te dira cuantas parejas debes encontrar
+Para elegir las parejas debes empezar desde el numero 0 hasta un número antes del numero de parejas
+esto sucede ya que al ser una lista empiezas a contar desde 0 y llegas hasta el final un número antes de la cantidad de parejas
+
+Al terminar la partida te saldra un mensaje con tu puntaje de la partida felicitandote.
--- Desktop/ProyectoPython/main.py
+++ Desktop/ProyectoPython/main.py
@@ -10,7 +10,8 @@ highestScore = 0
 #Creación de los pares de numeros aleatorios
 def createPairs():
     numbers = []
-    for _ in range(10):
+    numberOfPairs = random.randint(0,12)
+    for _ in range(numberOfPairs):
         num = random.randint(1,30)
         numbers.append(num)
         numbers.append(num)
@@ -26,30 +27,32 @@ def checkNumbers():
 #Comprobar si los indices dados contienen el mismo números
     while len(numbers) > 0 :
         
+        
         try:
-            a = int(input("Elige un número entre 0 y {}: \n \n".format(len(numbers))))
-            b = int(input("Elige otro número entre 0 y {}: \n \n".format(len(numbers))))
-            num1 = numbers[a]
-            num2 = numbers[b]
+            a = int(input("Elige un número entre 0 y {}: \n \n".format(len(numbers) - 1)))
+            b = int(input("Elige otro número entre 0 y {}: \n \n".format(len(numbers) - 1)))
 
-            if a == b:
-                print("Debes elegir 2 posiciones diferentes")
-                continue
             if a < 0 or b < 0 or a >= len(numbers) or b >= len(numbers):
                 print("Índices fuera de rango. Intenta de nuevo.")
                 continue
+            if a == b:
+                print("Debes elegir 2 posiciones diferentes")
+                continue
+
 
             num1 = numbers[a]
             num2 = numbers[b]
-
+    
             if num1 == num2:
         
-                print("elegiste", num1)
-                print("elegiste",num2)
+                print("El número es:", num1)
+                print("El número es:",num2)
                 score += 10
                 print("has encontrado una pareja! \n")
                 print("puntaje" , score ,"puntos \n")
                 print("has ganado 10 puntos")
+    
+    
     
     
         #Se remueven las parejas encontradas
@@ -58,11 +61,12 @@ def checkNumbers():
         #se iteran los números y elimina los elementos ya encontrados
                 numbers = [n for n in numbers if n is not None]
                 print("debes encontrar", round(len(numbers)/2), "parejas más")
+            
                 
     
             elif num1 != num2:
-                print(num1)
-                print(num2)
+                print("El número es:",num1)
+                print("El número es:",num2)
                 print("Los numeros no coinciden vuelve a intentar")
         except ValueError:
             print("Debes digitar números")
@@ -70,7 +74,7 @@ def checkNumbers():
     ##puntuación maxima
     if score > highestScore:
         highestScore = score
-        print("tu puntaje maximo es:")
+    
     
       
     
@@ -78,7 +82,7 @@ def checkNumbers():
 
 
 while(True):
-    print("Bienvenido a tu Memory Numbers \n")
+    print("\n Bienvenido a tu juego 'Memory Numbers' \n")
     print("Elige una opción:\n")
     print("Opción 1: Partida Nueva")
     print("Opción 2: Ver Puntuación Máxima")
@@ -91,7 +95,7 @@ while(True):
 
 
     elif answer == "2":
-        print("Tu puntaje máximo es de: ",highestScore)
+        print("Tu puntaje máximo es de: ",highestScore, "puntos")
     elif answer == "3":
         print("Gracias por haber jugado")
         break
